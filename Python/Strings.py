ff6313030f1dfea218af4a70764967b293edabd6
Manjil Bajgain <manjilsharma420@gmail.com>
2025-02-18 16:28:49 +0545

    Days2

---
+++ Python/Strings.py
@@ -0,0 +1,22 @@
+a= "Manjil"
+print(a)
+# since we know that string is the sequence of the character 
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+print(a[5])
+print(a[-1])
+print(a[-2])
+print(a[-3])
+print(a[-4])
+print(a[-5])
+print(a[-6])
+#By using for loop 
+for character in a:
+    print(a) 
+
+
+
+
