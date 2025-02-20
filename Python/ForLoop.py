135a550f819ea4bab927b5b7d9c4c761db2beb0d
Manjil Bajgain <manjilsharma420@gmail.com>
2025-02-20 21:40:30 +0545

    Days5

---
+++ Python/ForLoop.py
@@ -0,0 +1,20 @@
+x=["Hi","I","am","Manjil","Bajgain"]
+print("The list is :",x)
+for string in x:
+    print(string)
+    for j in string:
+        print(j)
+
+
+#while loop
+q=int(input("enter the number:"))
+while(q<=10):
+    print("Entering in the loop")
+    q=int(input("enter the number:"))
+    print(q)
+
+a=int(input("enter the number:"))
+while(True):
+    print("it is  not equal to 3:")
+    a=int(input("enter the number:"))
+    
