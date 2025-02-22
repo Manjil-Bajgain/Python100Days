7437d441ad63374f6ad1065b4fca2d635289f378
Manjil Bajgain <manjilsharma420@gmail.com>
2025-02-22 19:49:24 +0545

    Days7

---
+++ Python/Range.py
@@ -0,0 +1,11 @@
+for element in range(1,10):
+    print(element,end="")
+"""if we take 3 parameter in range then the first one is for starting 
+    point and second one is for ending point and last one is for gap 
+    interval between first point and last point in each iteration 
+    (in simple word add the last number in first number and so on
+      the iteration goes )"""
+#for example 
+for x in range(2,10,1):
+    print(x)
+
