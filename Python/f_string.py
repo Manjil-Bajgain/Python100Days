2a2d5d8a91e5e296774cef17b3a5e3b2a450f18d
Manjil Bajgain <manjilsharma420@gmail.com>
2025-02-25 20:43:09 +0545

    Day13

---
+++ Python/f_string.py
@@ -0,0 +1,18 @@
+#In previous version of of pyrhon the string formating is difficult. It required long code to formate the f-string
+#for example:
+intro1="Hi! , I am {} and i am from {},Bara" 
+intro="Hi! , I am {1} and i am from {0},Bara" 
+name="Manjil"
+city="Simara"
+print(intro.format(name,city))
+print(intro1.format(name,city))
+
+
+
+#Then to solve this long code format problem f-string is generated
+print(f"Hi! , I am {name} and I am from {city},Bara")
+f=float(208.393)
+print(f"the float value is : {f}")
+print(f"the float value is : {f:.1f}")
+#I can also print f-string like this 
+print(f"Hi!! , I am {{name}} and i am from {{city}}")  #use double curly bracket
