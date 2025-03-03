e3a6e31b5f6606af9c2ca5c6705ac5aa8a205501
Manjil Bajgain <manjilsharma420@gmail.com>
2025-03-03 13:29:14 +0545

    Day21

---
+++ Python/ForLoopWithElse.py
@@ -0,0 +1,12 @@
+for i in range(3):
+    print(i)
+    # if i==2:
+    #     break
+else:
+    print("No i is present:")
+
+for x in range(6):
+    print("The iteration of loop is :{} ".format(x+1))
+else:
+    print("else block in loop:")
+print("Out of loop:")
